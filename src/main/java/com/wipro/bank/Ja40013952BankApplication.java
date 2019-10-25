143595fc93ace9b901a23d67ee3ca8477640eaed
Jair Avilés <hi@jairaviles.mx>
2019-10-25 14:39:39 -0700

    Init commit

---
+++ src/main/java/com/wipro/bank/Ja40013952BankApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ja40013952BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ja40013952BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wipro/bank/Ja40013952BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ja40013952BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
